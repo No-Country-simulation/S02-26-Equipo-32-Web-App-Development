444e7b72e2ef1095a0d0cef931b4442f90bc9960
Maximo Cavallotto <187404300+MaximoCavallotto@users.noreply.github.com>
2026-02-10 12:38:38 -0300

    Feat: Implementacion completa del UserRepository y registro en DI

---
+++ Justina/Infraestruture/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interface.Repository;
+using Domain.Models;
+using Infraestruture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestruture.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly JustinaDbContext _context;
+
+        public UserRepository(JustinaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
