24296036d5e7a04b7389e8d8b32ce52a5913a61e
Maximo Cavallotto <187404300+MaximoCavallotto@users.noreply.github.com>
2026-02-10 14:04:30 -0300

    Feat: Endpoint de registro de usuarios (AuthController) implementado y probado

---
+++ Justina/Api/Controllers/Users/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos.Users;
+using Application.Interface.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // POST: api/Auth/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto dto)
+        {
+            try
+            {
+                int userId = await _authService.RegisterAsync(dto);
+                return Ok(new {Message = "Usuario registrado con éxito", UserId = userId });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+    }
+}
