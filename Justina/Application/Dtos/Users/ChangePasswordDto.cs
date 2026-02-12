5c66cf6b7079b075f4c8d7053eec5d204dde894e
Maximo Cavallotto <187404300+MaximoCavallotto@users.noreply.github.com>
2026-02-12 09:03:50 -0300

    Fix: Inicializacion de propiedades en ChangePasswordDto

--- Justina/Application/Dtos/Users/ChangePasswordDto.cs
+++ Justina/Application/Dtos/Users/ChangePasswordDto.cs
@@ -10,13 +10,13 @@ namespace Application.Dtos.Users
     public class ChangePasswordDto
     {
         [Required(ErrorMessage = "Debe ingresar su clave actual")]
-        public string CurrentPassword { get; set; }
+        public string CurrentPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Debe ingresar su nueva clave")]
-        public string NewPassword { get; set; }
+        public string NewPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Debe confirmar su nueva clave")]
         [Compare("NewPassword", ErrorMessage = "La confirmación de la nueva clave no coincide")]
-        public string ConfirmNewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; } = string.Empty;
     }
 }
