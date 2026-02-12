645d187e3ee932cb28b43818009f5ac1dded8695
Maximo Cavallotto <187404300+MaximoCavallotto@users.noreply.github.com>
2026-02-12 09:34:09 -0300

    Feat: Endpoint change-password en AuthController con extracción de Claims

--- Justina/Api/Controllers/Users/AuthController.cs
+++ Justina/Api/Controllers/Users/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Dtos.Users;
 using Application.Interface.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Api.Controllers.Users
 {
@@ -42,6 +44,26 @@ namespace Api.Controllers.Users
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        //POST: api/Auth/change-password
+        [HttpPost("change-password")]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto dto)
+        {
+            string? userEmail = HttpContext.User.FindFirst(ClaimTypes.Email)?.Value;
+            if (string.IsNullOrEmpty(userEmail))
+                return Unauthorized(new { Error = "No se pudo identificar al usuario desde el Token" });
+
+            try
+            {
+                await _authService.ChangePasswordAsync(dto, userEmail);
+                return Ok(new { Message = "Contraseña cambiada con éxito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
             }
         }
     }
